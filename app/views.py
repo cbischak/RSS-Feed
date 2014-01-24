13db4fffe9864ee7a51cb81fce488040ae2d7342
Christopher Bischak <cab3wz@virginia.edu>
2014-01-24 15:30:19 -0500

    Atom Feed
    This adds an Atom feed to the webpage

--- app/views.py
+++ app/views.py
@@ -2,11 +2,17 @@ from flask import render_template
 from app import app
 import feedparser
 d = feedparser.parse('http://www.reddit.com/r/news/.rss')
-
+e = feedparser.parse('http://bblfish.net/blog/blog.atom')
 @app.route('/')
 @app.route('/index')
 def index():
+    
     rss=[]
     for post in d.entries:
         rss.append(post.title+":"+post.link+"/n")
-    return render_template("index.html", title="Home", rss=rss)
+    
+    atom=[]
+    for post in e.entries:
+        atom.append(post.title+":"+post.link+"/n")
+    
+    return render_template("index.html", title="Home", rss=rss, atom=atom)

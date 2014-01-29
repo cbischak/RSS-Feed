444567a3171df7ab42f77d3c3f59dbc907cd671f
Christopher Bischak <cab3wz@virginia.edu>
2014-01-29 09:46:39 -0500

    Add a Feeder Button
    This adds a Feeder button and Feed Template

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from app import app
 import feedparser
-d = feedparser.parse('http://www.reddit.com/r/news/.rss')
-e = feedparser.parse('http://bblfish.net/blog/blog.atom')
+d = feedparser.parse('http://www.reddit.com/r/technology/.rss')
+e = feedparser.parse('http://blogs.oracle.com/main/feed/entries/atom')
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +16,6 @@ def index():
         atom.append(post.title+":"+post.link+"/n")
     
     return render_template("index.html", title="Home", rss=rss, atom=atom)
+@app.route('/feeder')
+def feed():
+    return render_template("feeder.html", title="feeder")
